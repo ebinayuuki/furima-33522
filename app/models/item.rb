7efb757d31f448720d1d8548525b811ba9cd54f8
ebinayuuki <tyuuzen1@yahoo.co.jp>
2021-01-21 18:53:02 +0900

    アクティブハッシュ導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,19 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belong_to :category
+  # , :status, :shipping_cost_burden, :prefecture, :shipping_day
+
   has_one_attached :image
+
+  with_options presence: true, numericality: { other_than: 1 } do
+    validates :category_id
+    # validates :status_id
+    # validates :shipping_cost_burden_id
+    # validates :prefecture_id
+    # validates :shipping_day_id
+
+  end
+
+
+
 end
