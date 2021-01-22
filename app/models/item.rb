09e2ab13b5c70e324fc7bd44ffe91af21ca44065
ebinayuuki <tyuuzen1@yahoo.co.jp>
2021-01-22 11:31:14 +0900

    出品機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,18 +1,23 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belong_to :category
-  # , :status, :shipping_cost_burden, :prefecture, :shipping_day
+  belongs_to :category, :status, :shipping_cost_burden, :prefecture, :shipping_day
 
   has_one_attached :image
 
-  with_options presence: true, numericality: { other_than: 1 } do
-    validates :category_id
-    # validates :status_id
-    # validates :shipping_cost_burden_id
-    # validates :prefecture_id
-    # validates :shipping_day_id
-
+  #商品出品アクティブハッシュのバリデーション
+  with_options presence: true, numericality: { other_than: 0 } do
+    validates :category_id, :status_id, :shipping_cost_burden_id, :prefecture_id, :shipping_day_id
   end
+
+  #商品出品入力フィールドのバリデーション
+  with_options presence: true do
+    validates :name, length: { maximum: 40 }  
+    validates :info, length: { maximum: 1000}
+    validates :price, inclusion: { in: 300..9999999 }, format: { with: /\A[0-9]+\z/}
+    validates :image
+  end
+
+
 
 
 
